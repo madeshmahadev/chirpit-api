68339be7deb87ede0608cdaebeb85cad7acbb9f3
mahadevkrishnamoorthy <m.krishnamoorthy@kineticarisk.com>
2023-08-18 23:15:28 -0400

    Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# Create some sample users
+user1 = User.create(email: 'user1@example.com', password: 'password')
+user2 = User.create(email: 'user2@example.com', password: 'password')
+
+# Create some sample chirps associated with users
+chirp1 = user1.api_v1_chirps.create(body: 'Sample chirp 1')
+chirp2 = user2.api_v1_chirps.create(body: 'Sample chirp 2')
